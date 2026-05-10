65ec6d45f163ec84595f3062fb30a733d264c05e
Rajdeep7424 <rajdeep.m.7424@gmail.com>
2026-05-10 18:43:40 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
